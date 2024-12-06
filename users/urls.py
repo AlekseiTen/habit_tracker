926476c441410fcbd09c5ec25274c6eb1af04248
Aleksei Ten <amailcn@mail.ru>
2024-12-06 10:32:10 +0300

    добавил урл до класса UserCreateAPIView.

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register"),
     path(
         "login/",
         TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
