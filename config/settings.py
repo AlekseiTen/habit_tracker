e91cec5b2000d682f6ab336cbb969714fe91dc20
Aleksei Ten <amailcn@mail.ru>
2024-12-06 10:02:56 +0300

    добавил настройки в файле settings.py для jwt

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -22,6 +23,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "habits",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -96,3 +98,15 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "UPDATE_LAST_LOGIN": True,  # отвечает за поле last_login чтобы было не Null
+}
